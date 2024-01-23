9c544c8697a2b5b49893e8e2dc176e2a2f36b441
stanislaus-onwuka <kachi2505@yahoo.com>
2024-01-23 13:36:40 +0100

    env removed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: '.env' })
 
 const DB = process.env.MONGO_URL.replace('<password>', process.env.MONGOPASSWORD);
 
